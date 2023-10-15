b6c47acdf06f9358c7bc1ee0e114af0f28ba9dc8
Monomythconvergence <shoulddoisuppose@gmail.com>
2023-10-15 21:51:34 +0300

    final

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,13 +1,39 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.Search_button)
+        val libraryButton = findViewById<Button>(R.id.Library_button)
+        val settingsButton = findViewById<Button>(R.id.Settings_button)
+
+
+
+
+        searchButton.setOnClickListener {
+
+            val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+        libraryButton.setOnClickListener {
+
+            val libraryIntent = Intent(this@MainActivity, LibraryActivity::class.java)
+            startActivity(libraryIntent)
+        }
+
+        settingsButton.setOnClickListener {
+
+            val settingsIntent = Intent(this@MainActivity, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
+
     }
 }
-
-asdasasa
