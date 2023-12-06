dd949584d6106f0dff1791684010cf2bd8bea810
Monomythconvergence <shoulddoisuppose@gmail.com>
2023-12-07 00:26:42 +0300

    Sprint9try1

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,74 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageButton
 
 class SearchActivity : AppCompatActivity() {
+
+    private var userInputReserve = ""
+
+    companion object {
+        const val USER_INPUT = "userInput"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(USER_INPUT, userInputReserve)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        userInputReserve = savedInstanceState.getString(USER_INPUT, "")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val searchBarField = findViewById<EditText>(R.id.searchBarField)
+        val searchBarClear = findViewById<ImageButton>(R.id.searchBarClear)
+
+        if (savedInstanceState != null) {
+            searchBarField.setText(userInputReserve)
+        }
+
+        searchBarField.addTextChangedListener(object : TextWatcher {
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Заглушка
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s?.isNotEmpty() == true) {
+                    searchBarClear.visibility = View.VISIBLE
+                } else {
+                    searchBarClear.visibility = View.INVISIBLE
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                userInputReserve = s.toString()
+            }
+        })
+
+        searchBarClear.setOnClickListener {
+            searchBarField.text.clear()
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(searchBarField.windowToken, 0)
+        }
+
+        val backButton = findViewById<ImageButton>(R.id.back_button)
+
+        backButton.setOnClickListener {
+
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,5 +1,7 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
@@ -16,5 +18,42 @@ class SettingsActivity : AppCompatActivity() {
             finish()
         }
 
+        val shareButton = findViewById<ImageButton>(R.id.settings_share_button)
+
+        shareButton.setOnClickListener {
+
+            val shareBody = getString(R.string.share_link)
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareBody)
+
+            val smsIntent = Intent(Intent.ACTION_VIEW)
+            smsIntent.data = Uri.parse("sms:")
+            val chooserIntent = Intent.createChooser(shareIntent, "Share via")
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(smsIntent))
+
+            startActivity(chooserIntent)
+        }
+        val supportButton = findViewById<ImageButton>(R.id.settings_support_button)
+
+        supportButton.setOnClickListener {
+
+
+
+            val sendIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, getString(R.string.email_recipient))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.email_subject))
+            }
+            startActivity(sendIntent)
     }
-}
+        val EULAButton = findViewById<ImageButton>(R.id.EULA_button)
+
+        EULAButton.setOnClickListener {
+
+            val link = getString(R.string.EULA_link)
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+            startActivity(intent)
+        }
+}}
