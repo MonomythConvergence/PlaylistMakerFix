2d20251ed89a68473a2fbd82c131df8fff73b099
Monomythconvergence <shoulddoisuppose@gmail.com>
2024-02-26 06:05:00 +0300

    Sprint13 Try3

--- app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
@@ -1,6 +1,6 @@
 package com.example.playlistmaker
 
-import android.content.Context
+
 import android.os.Bundle
 import android.util.Log
 import android.util.TypedValue
@@ -12,7 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.load.resource.bitmap.TransformationUtils.fitCenter
+
 
 class PlayerActivity : AppCompatActivity() {
 
@@ -38,11 +38,15 @@ class PlayerActivity : AppCompatActivity() {
 
         val artwork = findViewById<ImageView>(R.id.artwork)
         val roundingRadius = artwork.resources.getDimension(R.dimen.Artwork_rounding)
-        val pixelsForRoundedCorners = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, roundingRadius, artwork.resources.displayMetrics)
+        val pixelsForRoundedCorners = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_PX,
+            roundingRadius,
+            artwork.resources.displayMetrics
+        )
         Glide.with(artwork)
             .load(selectedTrack.artworkUrl100.replaceAfterLast('/', "512x512bb.jpg"))
             .placeholder(R.drawable.placeholder)
-            .transform(CenterCrop(),RoundedCorners(pixelsForRoundedCorners.toInt()))
+            .transform(CenterCrop(), RoundedCorners(pixelsForRoundedCorners.toInt()))
             .into(artwork)
         Log.d("MyTag", "Pixel count is ${pixelsForRoundedCorners.toInt()}")
         val trackName = findViewById<TextView>(R.id.trackName)
@@ -73,8 +77,10 @@ class PlayerActivity : AppCompatActivity() {
 
         val collectionName = findViewById<TextView>(R.id.collectionName)
         collectionName.text = selectedTrack.collectionName
-        if (selectedTrack.collectionName == "") {
+        val collectionNameField = findViewById<TextView>(R.id.collectionNameField)
+        if (selectedTrack.collectionName == "" || selectedTrack.collectionName == null) {
             collectionName.visibility = View.GONE
+            collectionNameField.visibility = View.GONE
         }
 
         val releaseDate = findViewById<TextView>(R.id.releaseDate)
@@ -84,6 +90,7 @@ class PlayerActivity : AppCompatActivity() {
 
         val country = findViewById<TextView>(R.id.country)
         country.text = selectedTrack.country
+
 
 
         val backButton = findViewById<View>(R.id.backButton)
--- app/src/main/java/com/example/playlistmaker/Tracks.kt
+++ app/src/main/java/com/example/playlistmaker/Tracks.kt
@@ -1,10 +1,11 @@
 package com.example.playlistmaker
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
 var trackList = arrayListOf<Track>()
 var recentTracksList = arrayListOf<Track>()
+@Parcelize
 class Track(
     val trackName: String,
     val artistName: String,
@@ -15,43 +16,4 @@ class Track(
     val releaseDate : String,
     val primaryGenreName : String,
     val country : String
-): Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readLong(),
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: ""
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(trackName)
-        parcel.writeString(artistName)
-        parcel.writeString(trackTime)
-        parcel.writeString(artworkUrl100)
-        parcel.writeLong(trackId)
-        parcel.writeString(collectionName)
-        parcel.writeString(releaseDate)
-        parcel.writeString(primaryGenreName)
-        parcel.writeString(country)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Track> {
-        override fun createFromParcel(parcel: Parcel): Track {
-            return Track(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Track?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+): Parcelable
